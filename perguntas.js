c20d816c94e8c19d096a231c2acb9f85112b1677
Davizinho <140724854+DaviSehnem@users.noreply.github.com>
2024-10-29 08:16:31 -0300

    29/10

---
+++ perguntas.js
@@ -0,0 +1,11 @@
+criaCartao(
+    'Matemática'
+    'Qual o valor de X? 2X + 60 = 3X . 5'
+    'X = 12'
+)
+criaCartao(
+    'historia'
+    'Quem descobriu as américas e o Brasil respectivamente?'
+    'Cristóvão Colombo e Pedro Álvares Cabral'
+)
+criaCartao
