1990b2a3fd214a6c41735785ddd746773f4a51c3
Davizinho <140724854+DaviSehnem@users.noreply.github.com>
2024-11-05 08:13:20 -0300

    05/11

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 function criaCartao(categoria, pergunta, resposta){
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
+    cartao.innerHTML = `
+     <div class="cartao_conteudo">
+                    <h3>${categoria}</h3>
+                    <div class="cartao_pergunta">
+                        <p>${pergunta}</p>
+                    </div>
+                    <div class="cartao_resposta"> 
+                        <p>${resposta}</p>
+                    </div>
+                </div>
+   `
 }
